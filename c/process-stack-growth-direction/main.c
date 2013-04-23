920fcf0d4ec3a6edca4eb5ab191675d9bd044440
Parveen Kumar <pkjain@gmail.com>
2013-04-23 16:03:34 +0530

    use recursion

--- c/process-stack-growth-direction/main.c
+++ c/process-stack-growth-direction/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void test_stack_growth_direction(int arg1, int arg2) {
-  int local_int1;
-  char *local_char_ptr1;
-  printf("%p\n%p\n%p\n%p\n", &arg1, &arg2, &local_int1, &local_char_ptr1);
+void test_stack_growth_direction(int arg1, int recursion_depth) {
+  printf("%p\n", &arg1);
+  if (recursion_depth < 10) {
+    test_stack_growth_direction(arg1 + 100, recursion_depth + 1);
+  }
 }
 
 main () {
-  test_stack_growth_direction(2, 3); 
+  test_stack_growth_direction(2, 0);
 }

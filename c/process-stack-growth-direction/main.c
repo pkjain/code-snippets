1af9f7e7fa1c36c1bed08ad07ecd3c907067927d
Parveen Kumar <pkjain@gmail.com>
2013-04-23 16:19:42 +0530

    use local var

--- c/process-stack-growth-direction/main.c
+++ c/process-stack-growth-direction/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void test_stack_growth_direction(int arg1, int recursion_depth) {
-  printf("%p\n", &arg1);
+void test_stack_growth_direction(recursion_depth) {
+  int local_int1;
+  printf("%p\n", &local_int1);
   if (recursion_depth < 10) {
-    test_stack_growth_direction(arg1 + 100, recursion_depth + 1);
+    test_stack_growth_direction(recursion_depth + 1);
   }
 }
 
 main () {
-  test_stack_growth_direction(2, 0);
+  test_stack_growth_direction(0);
 }
